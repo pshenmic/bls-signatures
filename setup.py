39c431214261dbf9cc1a1b3315942c697e0f304e
pshenmic <pshenmic@gmail.com>
2024-01-16 20:24:38 +0400

    Bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import subprocess
 import sys
 from distutils.version import LooseVersion
 
-from setuptools import Extension, setup, setuptools
+from setuptools import Extension, setup
 from setuptools.command.build_ext import build_ext
 
 
